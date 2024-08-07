6061aea1c1350583a6d4a3c7f9ca5fba42bbc910
haiyang <1919131045@qq.com>
2024-08-07 15:00:57 +0800

    加入前端vue

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory, abort
+import os
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='static/dist', template_folder='static/dist')
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return send_from_directory(app.template_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_vue(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # 如果路径不存在或为空，则返回 index.html
+        return send_from_directory(app.template_folder, 'index.html')
 
 
 if __name__ == '__main__':
